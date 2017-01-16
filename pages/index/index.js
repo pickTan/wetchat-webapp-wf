28e1834f2c15b154e9caa9a39de52960942863df
pengfeng@just-tech.com.cn <picktan>
2017-01-16 18:10:52 +0800

    提交小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,42 +17,47 @@ Page({
                 link:`/pages/proInfo/index?id=93`,  
                 url:`${rootUrl}/images/upload/index_list_ban_093.jpg` ,
                 text:'洁面乳+天才水+玻尿酸精华',
-                title:'年终特惠价￥158'
+                title:'年终特惠价￥158.00'
           },{
               link:`/pages/proInfo/index?id=86`,  
                 url:`${rootUrl}/images/upload/index_list_ban_086.jpg` ,
                 text:'白竹炭净颜洗面奶男女通用100ML',
-                title:'销售价￥58'
+                title:'销售价￥58.00'
+          },{
+              link:`/pages/proInfo/index?id=88`,  
+                url:`${rootUrl}/images/upload/index_list_ban_088.jpg` ,
+                text:'玻尿酸精华液25ML',
+                title:'销售价￥109.00'
           },{
               link:`/pages/proInfo/index?id=87`,  
                 url:`${rootUrl}/images/upload/index_list_ban_087.jpg` ,
-                text:'玻尿酸精华液25ML',
-                title:'销售价￥109'
+                text:'青春密码天才水爽肤水125ML',
+                title:'销售价￥109.00'
           },{
               link:`/pages/proInfo/index?id=85`,  
                 url:`${rootUrl}/images/upload/index_list_ban_085.jpg` ,
                 text:'蓝莓水盈深润蚕丝面膜25ML*6片装',
-                title:'销售价￥49.9'
+                title:'销售价￥49.90'
           },{
               link:`/pages/proInfo/index?id=84`,  
                 url:`${rootUrl}/images/upload/index_list_ban_084.jpg` ,
                 text:'玫瑰提亮净颜蚕丝面膜25ML*6片装',
-                title:'销售价￥49.9'
+                title:'销售价￥49.90'
           },{
               link:`/pages/proInfo/index?id=82`,  
                 url:`${rootUrl}/images/upload/index_list_ban_082.jpg` ,
                 text:'水灵灵密集保湿蚕丝面膜30ML*6片装',
-                title:'销售价￥59.9'
+                title:'销售价￥59.90'
           },{
               link:`/pages/proInfo/index?id=81`,  
                 url:`${rootUrl}/images/upload/index_list_ban_081.jpg` ,
                 text:'鲜嫩嫩舒缓修复蚕丝面膜30ML*6片装',
-                title:'销售价￥59.9'
+                title:'销售价￥59.90'
           },{
               link:`/pages/proInfo/index?id=95`,  
                 url:`${rootUrl}/images/upload/index_list_ban_095.jpg` ,
                 text:'天杞园特殊膳食 科学 安全 有效 不反弹',
-                title:'销售价￥198'
+                title:'销售价￥198.00'
           }
       ],  
         indicatorDots: true,
